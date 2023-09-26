fd2e8eba944864d5406d3af64c502f0dbdccb3cc
Vishal97prajapati <vishalprajapati097@gmail.com>
2023-09-26 14:34:57 +0530

    this is my firt commit

---
+++ index1.js
@@ -0,0 +1,11 @@
+console.log("hello vishal ");
+
+function add (a , b){
+    return a+b;
+
+}
+console.log(add( 2,8));
+ console.log(process.argv);
+
+var args = process.argv.slice(2);
+console.log("adding the number:", add(parseInt(args[0]), parseInt(args [1])));
